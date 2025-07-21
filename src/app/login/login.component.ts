8e9012211cb5cc2748b65dd656bc474ceb3dd06d
teedeenk <tsele@nkolex.co.za>
2025-07-21 14:57:41 +0200

    Changed the login back button to only go back to home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,32 +1,29 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  showPassword = false;
 
-    showPassword = false;
-
-    formData = {
+  formData = {
     username: '',
-    password: ''
+    password: '',
   };
 
-  constructor(private location: Location) {}
-    togglePassword() {
+  constructor(private router: Router) {}
+  togglePassword() {
     this.showPassword = !this.showPassword;
   }
-    onSubmit() {
-    }
+  onSubmit() {}
 
   goBack() {
-    this.location.back();
+    this.router.navigate(['/']);
   }
 }
